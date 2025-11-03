df572e31d66e02c46e2e17f5d72f906c1d8cbc5d
Avery Williams <awilliams@chapman.edu>
2025-11-03 19:50:42 +0000

    cars

---
+++ cars.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# cars.sh
+# Avery Williams
+# CENG 298
+
+echo "Welcome! Here are your choices"
+
+while true; do
+    echo "1. Add a car"
+    echo "2. List cars in inventory"
+    echo "3. Quit"
+    read -r -p "What would you like to do? " user_choice
+    case $user_choice in
+        1)
+            read -r -p "What year was the car created? " car_year
+            read -r -p "Who made the car? " car_creator
+            read -r -p "What model is the car? " car_model
+            echo "Ok, we have registered your vehicle: $car_year $car_creator $car_model"
+            ;;
+        2)
+            echo "Ok, your car is a $car_year $car_creator $car_model"
+            ;;
+        3)
+            echo "Ok, goodbye!"
+            break
+            ;;
+        *)
+            echo "Not an option"
+            ;;
+    esac
+done
